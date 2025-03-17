5b0f9d79a893d494d9153c11b0f0fba42e575193
Orest Vaheman <ovaheman@gmail.com>
2025-03-17 13:58:16 +0200

    fix hardcoding

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const price = 40 * days;
+  const pricePerDays = 40 * days;
+  const midDiscount = 20;
+  const bigDiscount = 50;
 
   if (days >= 7) {
-    return price - 50;
+    return pricePerDays - bigDiscount;
   }
 
   if (days >= 3) {
-    return price - 20;
+    return pricePerDays - midDiscount;
   }
 
-  return price;
+  return pricePerDays;
 }
 
 module.exports = calculateRentalCost;
